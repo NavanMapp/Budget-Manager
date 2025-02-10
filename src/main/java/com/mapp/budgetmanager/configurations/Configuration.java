abecbe2985ab673d6d6b513fe3bad117291d0acd
NavanMapp <navanmapp@gmail.com>
2025-02-10 22:58:55 +0200

    Configuration files updated

--- src/main/java/com/mapp/budgetmanager/configurations/Configuration.java
+++ src/main/java/com/mapp/budgetmanager/configurations/Configuration.java
@@ -1,22 +1,28 @@
 package com.mapp.budgetmanager.configurations;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @org.springframework.context.annotation.Configuration
-public class Configuration {
+public class Configuration implements WebMvcConfigurer {
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                WebMvcConfigurer.super.addCorsMappings(registry);
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedOrigins("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("*");
-            }
-        };
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .cors().and()
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll()
+                );
+        return http.build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedOrigins("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("*");
     }
 }
---
+++ src/main/java/com/mapp/budgetmanager/configurations/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.mapp.budgetmanager.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+    }
+}
